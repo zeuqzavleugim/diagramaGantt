7cd8202a10bd794c40192d089d6f79309f00e5d8
zeuqzavleugim <miguelvazquez870@gmail.com>
2023-09-21 18:19:55 -0600

    add  and changes code
    para hacerlo mas funcional con los requerimientos solicitados se han realizado cambios para agregar nombres

--- javaScrip.js
+++ javaScrip.js
@@ -22,13 +22,20 @@ class Gantt {
       var minDates = [];
   
       for(let i = 0; i < this.tasks.length; i++){
-        minDates.push(new Date(this.tasks[i][1]));
-        maxDates.push(new Date(this.tasks[i][2]));     
+        minDates.push(new Date(this.tasks[i][2]));
+        maxDates.push(new Date(this.tasks[i][3]));     
       }
       this.minDate = new Date(Math.min.apply(null,minDates));
       this.maxDate = new Date(Math.max.apply(null,maxDates)); 
   
     }   
+    setName(){
+      var name = [];
+  
+      for(let i = 0; i < this.tasks.length; i++){
+        name.push(this.tasks[i][0]);     
+      }
+    }
   
   
     /**
@@ -36,7 +43,7 @@ class Gantt {
     * @returns {Sting} Html code
     */
     buildTableHeader(){
-      var html = '<table><thead><tr>';
+      var html = '<table><thead><tr><th>Nombre</th>';
       var diffDays = this.diffInDays(this.maxDate, this.minDate) + 1;
       const actual = new Date(this.minDate);  
   
@@ -60,8 +67,10 @@ class Gantt {
       for(let i = 0; i < this.tasks.length; i++){
         var task = this.tasks[i]; 
   
-        var dMin = new Date(task[1]);
-        var dMax = new Date(task[2]);     
+        var name = task[0];
+
+        var dMin = new Date(task[2]);
+        var dMax = new Date(task[3]);     
   
         var days = this.diffInDays(dMax, dMin) + 1;
         var daysBefore = this.diffInDays(this.minDate, dMin);
@@ -71,8 +80,9 @@ class Gantt {
         if(this.maxDate == dMax) daysAfter = 0;
   
         html += '<tr>';
+        html += '<th>' + name +'</th>';
         if(daysBefore > 0) for(let j = 0; j < daysBefore; j++) html += '<td></td>';
-        html += '<td class="event-cell" colspan="'+days+'" style="background-color: '+task[3]+';"><span>'+task[4]+'% terminado</span>'+task[0]+'</td>';
+        html += '<td class="event-cell" colspan="'+days+'" style="background-color: '+task[4]+';"><span>'+task[5]+'% terminado</span>'+task[1]+'</td>';
         if(daysAfter > 0) for(let j = 0; j < daysAfter; j++) html += '<td></td>';
         html += '</tr>';
       }
@@ -101,12 +111,13 @@ class Gantt {
   function gantt(){
     
     var obj = new Gantt([
-        ['Action 1', '2023-05-12', '2023-05-13', '#4287f5', 80],
-				['Action 2', '2023-05-12', '2023-05-14', '#c1409b', 10],
-				['Action 3', '2023-05-14', '2023-05-17', '#0b9971', 20],
-				['Action 4', '2023-05-18', '2023-05-22', '#d26a52', 55],
-				['Action 5', '2023-05-19', '2023-05-22', '#4287f5', 100],
-				['Action 6', '2023-05-12', '2023-05-20', '#0b9971', 32],
+        ['Raul', 'Action 1', '2023-05-12', '2023-05-13', '#4287f5', 80],
+				['Sofia', 'Action 2', '2023-05-12', '2023-05-14', '#c1409b', 10],
+				['Rafa', 'Action 3', '2023-05-14', '2023-05-17', '#0b9971', 20],
+				['Laura', 'Action 4', '2023-05-18', '2023-05-22', '#d26a52', 55],
+				['Karla', 'Action 5', '2023-05-19', '2023-05-22', '#4287f5', 100],
+				['Martin', 'Action 6', '2023-05-12', '2023-05-20', '#0b9971', 32]
+      //['0       ', '1         ', '2         ', '3      ','4      ','5']
 				]);
     
   }
